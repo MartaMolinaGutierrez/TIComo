89a8cf43c5eab30aaef7d7db1904d8b15f51a571
MARTA MOLINA GUTIÉRREZ <marta.molina9@alu.uclm.es>
2022-10-13 16:51:47 +0200

    Init project

---
+++ TIComo/src/main/java/uclm/esi/equipo01/TIComo/TiComoApplication.java
@@ -0,0 +1,13 @@
+package uclm.esi.equipo01.TIComo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiComoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiComoApplication.class, args);
+	}
+
+}
---
+++ TIComo/src/test/java/uclm/esi/equipo01/TIComo/TiComoApplicationTests.java
@@ -0,0 +1,13 @@
+package uclm.esi.equipo01.TIComo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiComoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
